a01a547ae752ae42355e1d6f610b73e2fc4cc05a
camilosanchezalfaro <sanchezalfarocamilo@gmail.com>
2024-12-15 19:41:58 -0300

    Update WebConfig.js

--- models/WebConfig.js
+++ models/WebConfig.js
@@ -1,1 +1,27 @@
+const mongoose = require('mongoose');
+
+const WebConfigSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true,
+  },
+  selectorPronosticos: {
+    type: String,
+    required: true,
+  },
+  selectorFecha: {
+    type: String,
+    required: true,
+  },
+  selectorTitulos: {
+    type: String,
+    required: true,
+  },
+  palabrasClave: {
+    type: [String],
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('WebConfig', WebConfigSchema);
 
